22affbd51bc0121d789e70d4abe9aad471daf4b0
Mark Lawson <mark@walkonmars.co.uk>
2022-06-21 16:57:59 +0100

    reinstalled sass

--- src/components/FilterList/FilterList.jsx
+++ src/components/FilterList/FilterList.jsx
@@ -5,6 +5,7 @@ import FilterItem from "../FilterItem/FilterItem";
 const FilterList = ({handleChecked, filters}) => {
   return (
     <div className="filter-list">
+      <h2 className="filter-list__heading">Filter Your Search</h2>
       {filters.map((filter, index) => {
         return <FilterItem filterLabel={filter} key={index} index={index} handleChecked={handleChecked} />;
       })}
