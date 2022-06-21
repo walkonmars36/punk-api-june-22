09dd26806ac421edac9dc4727972b1ef1f6b18f4
Mark Lawson <mark@walkonmars.co.uk>
2022-06-21 09:04:06 +0100

    styling inputs

--- src/components/CardFront/CardFront.jsx
+++ src/components/CardFront/CardFront.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import "./CardFront.scss";
 
 const Card = ({beers}) => {
-  const {name, abv, image_url} = beers;
+  const {name, abv, image_url, tagline, first_brewed} = beers;
   return (
     <div className="card-front">
       <div className="card-front__content">
         <img className="card-front__img" src={image_url} alt="" />
         <p>{name}</p>
-        <p>{abv}</p>
+        <p>{tagline}</p>
+        <p>First Brewed - {first_brewed}</p>
+        <p>ABV - {abv}</p>
       </div>
     </div>
   );
