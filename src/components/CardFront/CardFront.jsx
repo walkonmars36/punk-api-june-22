403e4a83bc58d981b17ce52f1fd62d49410ad166
Mark Lawson <mark@walkonmars.co.uk>
2022-06-21 21:33:53 +0100

    added conditional rendering to card

--- src/components/CardFront/CardFront.jsx
+++ src/components/CardFront/CardFront.jsx
@@ -1,17 +1,43 @@
 import React from "react";
+import {useState} from "react";
 import "./CardFront.scss";
 
 const Card = ({beers}) => {
-  const {name, abv, image_url, tagline, first_brewed} = beers;
+  const {name, abv, image_url, tagline, first_brewed, description} = beers;
+  const [showText, setShowText] = useState(false);
+
+  const handleClick = () => {
+    setShowText(!showText);
+  };
+
+  const cardFrontJSX = (
+    <div className="card-front__content">
+      <img className="card-front__img" src={image_url} alt="" />
+      <h3 className="card-front__heading">{name}</h3>
+      <h4 className="card-front__tagline">{tagline}</h4>
+      <p className="card-front__first-brewed">First Brewed - {first_brewed}</p>
+      <p className="card-front__abv">ABV - {abv}</p>
+      <button onClick={handleClick}>Learn More</button>
+    </div>
+  );
+
+  const cardBackJSX = (
+    <div className="card-front__content">
+      <div>{description}</div>
+      <button onClick={handleClick}>Close</button>
+    </div>
+  );
+
   return (
     <div className="card-front">
-      <div className="card-front__content">
+      {/* <div className="card-front__content">
         <img className="card-front__img" src={image_url} alt="" />
         <h3 className="card-front__heading">{name}</h3>
         <h4 className="card-front__tagline">{tagline}</h4>
         <p className="card-front__first-brewed">First Brewed - {first_brewed}</p>
         <p className="card-front__abv">ABV - {abv}</p>
-      </div>
+      </div> */}
+      {showText ? cardBackJSX : cardFrontJSX}
     </div>
   );
 };
